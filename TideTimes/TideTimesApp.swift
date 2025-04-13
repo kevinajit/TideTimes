139cb5cedc2e515eff6b4f3164b1e2c8f9afde20
Kevin Ajit <kevinajit@icloud.com>
2025-04-13 21:46:49 +0530

    Initial Commit

---
+++ TideTimes/TideTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideTimesApp.swift
+//  TideTimes
+//
+//  Created by Kevin Ajit on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TideTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
